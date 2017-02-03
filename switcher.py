6124027200dd17073104d596330abc62885a1588
Jeremiah Richter <jeremiahrichter@gmail.com>
2017-02-03 11:33:27 -0600

    print out profile listing

--- switcher.py
+++ switcher.py
@@ -2,10 +2,13 @@
 
 import subprocess
 
-print('Choose a profile to switch to:')
+print('Enter the number of a profile to switch to:')
 
 results = subprocess.run(["netctl", "list"], stdout=subprocess.PIPE)
 
-processed_output = [x.strip() for x in results.stdout.decode('utf-8').split('\n')]
+processed_output = [x.strip() for x in results.stdout.decode('utf-8').split('\n') if x != '']
 
-print(processed_output)
+printable_list = [(x, False) if x[0] != '*' else (x[2:], True) for x in processed_output]
+
+for i, profile in enumerate(printable_list):
+  print('{}: {} {}'.format(i, profile[0], '[active]' if profile[1] == True else ''))
