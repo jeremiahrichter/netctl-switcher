876f3f92f776202d8639251d38c214810097f45e
Jeremiah Richter <jeremiahrichter@gmail.com>
2017-02-03 11:38:20 -0600

    accept user input

--- switcher.py
+++ switcher.py
@@ -2,8 +2,6 @@
 
 import subprocess
 
-print('Enter the number of a profile to switch to:')
-
 results = subprocess.run(["netctl", "list"], stdout=subprocess.PIPE)
 
 processed_output = [x.strip() for x in results.stdout.decode('utf-8').split('\n') if x != '']
@@ -12,3 +10,11 @@ printable_list = [(x, False) if x[0] != '*' else (x[2:], True) for x in processe
 
 for i, profile in enumerate(printable_list):
   print('{}: {} {}'.format(i, profile[0], '[active]' if profile[1] == True else ''))
+
+while True:
+  try:
+    text = input('Enter profile #, "q" to exit: ')
+    if text.lower() == 'q':
+      break
+  except ValueError:
+    print('Not a number')
